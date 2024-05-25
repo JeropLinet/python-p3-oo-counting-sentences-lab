d79daca2814301983635f89bbd60cb8c3129182a
Linet <teweilinet@gmail.com>
2024-05-25 14:02:08 +0300

    counting sentences

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self,value=""):
+   self.value=value
+
+  def value_string(self):
+    if not isinstance(self.value,str):
+      print("The value must be a string.")
+    
+  def is_sentence(self):
+        return self.value.endswith(".")
+
+  def is_question(self):
+        return self.value.endswith("?")
+
+  def is_exclamation(self):
+        return self.value.endswith("!")
+ 
